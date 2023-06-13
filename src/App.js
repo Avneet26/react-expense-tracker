48abd95e7fefcbf0412dd53044f2656593627ac0
Avneet Singh Virdi <avneetvirdi26@gmail.com>
2023-06-13 08:28:29 +0530

    some hooks written

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Dashboard from "./Components/Dashboard";
 import Header from "./Components/Header";
 import History from "./Components/History";
@@ -10,6 +10,31 @@ function App() {
     { id: 3, name: "Rent", amount: -250 },
     { id: 4, name: "Stocks", amount: 350 },
   ]);
+
+  const [balance, setBalance] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [totalExpense, setTotalExpense] = useState(0);
+
+  const handleCalculation = () => {
+    let balance = 0;
+    let totalIncome = 0;
+    let totalExpense = 0;
+
+    expenses.forEach((expense) => {
+      if (expense.amount > 0) {
+        totalIncome += expense.amount;
+      } else {
+        totalExpense += expense.amount;
+      }
+    });
+
+    balance = totalIncome + totalExpense;
+    return {balance, totalIncome, totalExpense};
+  };
+
+  useEffect(() => {
+    console.log(handleCalculation());
+  }, [expenses]);
 
   return (
     <div className="App">
